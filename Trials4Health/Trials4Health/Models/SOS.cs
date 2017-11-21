ef86422663e31e891cf2c2546146a8555fe8602a
Marco Vieira <32910967+marcovieira2710@users.noreply.github.com>
2017-11-21 16:51:36 +0000

    Criação da Class SOS, Alteração das Views, Liagação dos botões com o Controler, para assim visualizar as Views dos Primeiros Socorros.
    Tempo:  2h

---
+++ Trials4Health/Trials4Health/Models/SOS.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Trials4Health.Models
+{
+
+    public class SOS
+    {
+
+        int ID_SOS { get; set; }
+        string Nome_SOS { get; set; }
+        string Descrição { get; set; }
+
+    }
+}
