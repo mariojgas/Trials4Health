1c71d91744cfaa18ad3e6b50debba7b596f1f6af
Mario <mariojgas@gmail.com>
2018-01-10 23:31:06 +0000

    Criar controlador das estatisticas 2h

---
+++ Trials4Health/Trials4Health/Controllers/EstatisticasController.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Trials4Health.Models;
+
+namespace Trials4Health.Controllers
+{
+    public class EstatisticasController : Controller
+    {
+
+        private RepositorioTrails repository;
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public EstatisticasController(RepositorioTrails repository)
+        {
+            this.repository = repository;
+        }
+
+        public ViewResult Estatistica() => View(repository);
+
+        [HttpGet]
+        public ViewResult Rsvp()
+        {
+            return View();
+        }
+
+
+        public async Task<IActionResult> XS()
+        {
+            return View(await repository.Trilhos.ToListAsync());
+        }
+
+        public async Task<IActionResult> BuscarTrilho(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Trilho t = await repository.Trilhos.SingleOrDefaultAsync(tr => tr.Id == id);
+            if (t == null)
+            {
+                return NotFound();
+            }
+
+            return  View(t);
+        }
+        //
+        // POST: /Home/Rsvp
+        [HttpPost]
+        public ViewResult Rsvp(Trilho t)
+        {
+            if (ModelState.IsValid)
+            {
+                //Repository.AddResponse(response);
+
+                return View("EstatisticasTrilho", t);
+            }
+            else
+            {
+                // There are validation errors
+                return View();
+            }
+        }
+
+    }
+}
