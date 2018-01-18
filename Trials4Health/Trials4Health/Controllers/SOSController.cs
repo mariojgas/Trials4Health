f34e9311a60d018cd7f72d86d55f375205d1dd73
marcovieira2710 <32910967+marcovieira2710@users.noreply.github.com>
2018-01-18 16:01:02 +0000

    Controlador SOS, atualizado

---
+++ Trials4Health/Trials4Health/Controllers/SOSController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Trials4Health.Models;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+
+
+
+namespace Trials4Health.Controllers
+{
+    public class SOSController : Controller
+    {
+        private IRepositorioTrails repository;
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public SOSController(IRepositorioTrails repository)
+        {
+            this.repository = repository;
+        }
+
+        public ViewResult PrimeirosSocorros() => View();
+        public ViewResult Desmaios() => View(repository.PrimeirosSocorros);
+        public ViewResult Entorses() => View(repository.PrimeirosSocorros);
+        public ViewResult Hipotermia() => View(repository.PrimeirosSocorros);
+        public ViewResult Ataque() => View(repository.PrimeirosSocorros);
+    }
+}
